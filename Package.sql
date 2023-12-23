929c40c7a2eca997fd70bf2ba973cd41a5f30486
Pilonnus <114503037+Pilonnus@users.noreply.github.com>
2023-12-23 14:26:02 +0100

    Update Package.sql

--- Package.sql
+++ Package.sql
@@ -8,10 +8,14 @@ PROCEDURE connexion(idUtilisateur IN VARCHAR);
 PROCEDURE deconnexion;
 PROCEDURE ajouterAmi(idAmi IN VARCHAR);
 PROCEDURE supprimerAmi(idAmi IN VARCHAR);
+PROCEDURE compterAmi (idUtilisateur IN VARCHAR);
+PROCEDURE afficherAmi(loginUtilisateur IN VARCHAR)
 PROCEDURE afficherMur(idUtilisateur IN VARCHAR);
 PROCEDURE ajouterMessageMur(message IN VARCHAR);
 PROCEDURE supprimerMessageMur(message_id IN VARCHAR);
-PROCEDURE afficherAmi(loginUtilisateur IN VARCHAR);
+PROCEDURE repondreMessageMur(idMessage1 IN NUMBER, messageReponse IN VARCHAR);
+PROCEDURE chercherMembre(IN prefixeUtilisateur IN VARCHAR);
+
 
 -- Varaible publique
 utilisateurConnecte varchar(20);
