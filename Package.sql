f5744d78c656a27e5c6c5b4e4a000f865248c90d
Arthur <151721769+ArthurM3il@users.noreply.github.com>
2023-12-23 13:30:25 +0100

    procedure afficherAmi

--- Package.sql
+++ Package.sql
@@ -10,7 +10,8 @@ PROCEDURE ajouterAmi(idAmi IN VARCHAR);
 PROCEDURE supprimerAmi(idAmi IN VARCHAR);
 PROCEDURE afficherMur(idUtilisateur IN VARCHAR);
 PROCEDURE ajouterMessageMur(message IN VARCHAR);
-PROCEDURE supprimerMessageMur(message_id IN VARCHAR)
+PROCEDURE supprimerMessageMur(message_id IN VARCHAR);
+PROCEDURE afficherAmi(loginUtilisateur IN VARCHAR);
 
 -- Varaible publique
 utilisateurConnecte varchar(20);
